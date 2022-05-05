9e9a41c85dad2e5b8a6fc02b3993727521a4e594
BenjaminTomczyk <benjamin.tomczyk.auditeur@lecnam.net>
2022-05-05 11:24:45 +0200

    Debut PlayerController et GameController

--- Battleship/BattleshipAPI/PlayerController.cs
+++ Battleship/BattleshipAPI/PlayerController.cs
@@ -3,42 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Battleship.Model.Entities;
-using System.Net;
 
-namespace BattleshipAPI.Controllers
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BattleshipAPI
 {
     [Route("api/[controller]")]
-    public class GameController : Controller
+    public class PlayerController : Controller
     {
         // GET: api/values
-        [Produces("text/html")]
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public IEnumerable<string> Get()
         {
-            try
-            {
-                var content = "<b><i><u>Hello World</u></i></b>";
-                return new ContentResult
-                {
-                    Content = content,
-                    ContentType = "text/html",
-                    StatusCode = (int)HttpStatusCode.OK
-                };
-            }
-            catch
-            {
-                return BadRequest();
-            }
+            return new string[] { "player1", "player2" };
         }
-
-
 
         // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "player " + id;
         }
 
         // POST api/values
