8247514e548ea0e61728fddebbec460262cb32d8
BenjaminTomczyk <benjamin.tomczyk.auditeur@lecnam.net>
2022-05-09 20:21:28 +0200

    Profil joueur terminé (sans historique des parties)

--- Battleship/BattleshipAPI/wwwroot/js/Script.js
+++ Battleship/BattleshipAPI/wwwroot/js/Script.js
@@ -1,4 +1,28 @@
-﻿function getUser() {
+﻿function auth() {
+    if(sessionStorage.user == undefined){
+        window.alert("Vous n'êtes pas connectés");
+        window.location.assign("index.html");
+    }
+}
+
+function back() {
+    auth();
+    history.back();
+}
+
+function disconnect() {
+    auth();
+    sessionStorage.removeItem("user");
+    window.location.assign("index.html");
+}
+
+function loadProfile() {
+    auth();
+    window.location.assign("Profil.html");
+}
+
+function getUser() {
+    auth();
     var user = JSON.parse(sessionStorage.user);
 
     fetch('api/users/' + user.id, {
@@ -8,19 +32,30 @@
             'Content-Type': 'application/json'
         }
     })
-    .then(function (response) {
-        if (response.status == 401) {
-            window.alert(response.statusText+ " - Accès non autorisé !");
+    .then(response => response.json())
+    .then((response) => {
+        document.getElementById('LastName').innerHTML= response.lastName;
+        document.getElementById('FirstName').innerHTML= response.firstName;
+        document.getElementById('UserName').innerHTML= response.userName;
+        document.getElementById('Email').innerHTML= response.email;
+        if(Array.from(user.roles).includes("Administrator")){
+            document.getElementById('Role').innerHTML= "Administrateur";
         }
-        else if (response.status == 200) {
-            window.location.replace("Profil.html");
+        else {
+            document.getElementById('Role').innerHTML= "Joueur";
         }
+        
     })
     .catch(error => console.error('Error ', error));
 }
 
 
 function game(res) {
+    if (arguments.length == 0) {
+        auth();
+        res = JSON.parse(sessionStorage.user);
+    }
+
     fetch('api/game', {
         headers: {
             'Authorization': 'Bearer ' + res.token
@@ -28,7 +63,7 @@ function game(res) {
     })
         .then(function (response) {
             if (response.status == 200) {
-                window.location.replace("Game.html");
+                window.location.assign("Game.html");
                 sessionStorage.setItem("user",JSON.stringify(res))
             }
             else {
@@ -71,7 +106,7 @@ function token(mail, pwd) {
         .then((response) => {
             if (response.isAuthenticated == false) {
                 window.alert(response.message);
-                password.value = '';
+                document.getElementById('Password').value = '';
             }
             else if (response.isAuthenticated) {
                 game(response);
@@ -82,17 +117,14 @@ function token(mail, pwd) {
 
 
 function register() {
-    const lastName = document.getElementById('LastName');
-    const firstName = document.getElementById('FirstName');
-    const userName = document.getElementById('UserName');
-    const email = document.getElementById('Email1');
-    const password = document.getElementById('Password1');
+    email = document.getElementById('Email1');
+    password = document.getElementById('Password1');
 
     const item = {
         isComplete: false,
-        LastName: lastName.value.trim(),
-        FirstName: firstName.value.trim(),
-        UserName: userName.value.trim(),
+        LastName: document.getElementById('LastName').value.trim(),
+        FirstName: document.getElementById('FirstName').value.trim(),
+        UserName: document.getElementById('UserName').value.trim(),
         Email: email.value.trim(),
         Password: password.value.trim()
     };
@@ -110,6 +142,9 @@ function register() {
             window.alert(response);
             if (response == "L'e-mail est déjà enregistré") {
                 email.value = '';
+                password.value = '';
+            }
+            else if (response.includes("Mot de passe trop faible")){
                 password.value = '';
             }
             else if (response == "Inscription validée") {
